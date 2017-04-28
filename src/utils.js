2ea9140a93a4fe3c8efc5405bcc08cf876b65685
jkzing <jingkai.zhao@foxmail.com>
2017-04-28 11:24:41 +0800

    build book

--- src/utils.js
+++
@@ -1,9 +0,0 @@
-export function assert(condition, message) {
-  if (!condition) throw new Error(message);
-}
-
-export function warning(condition, message) {
-  if (!condition) {
-    console.warn(`[mobx-weapp]: ${message}`);
-  }
-}
