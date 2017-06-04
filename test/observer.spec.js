f347adefb5a6ebae4a6bdb8334f3b59e324024f8
jkzing <jingkai.zhao@foxmail.com>
2017-06-05 00:32:01 +0800

    add test cases for observer

--- test/observer.spec.js
+++ test/observer.spec.js
@@ -2,7 +2,33 @@ import assert from 'assert';
 import observer from '../src/observer';
 
 describe('observer', () => {
+
+    let options;
+    beforeEach(() => {
+        global.Page = jest.fn();
+        options = {
+            setData: jest.fn(),
+        };
+    });
+
     it('exposes observer function', () => {
         expect(typeof observer).toBe('function');
+    });
+
+    it('creates proxy methods for onLoad and onUnload', () => {
+        let mapState = store => ({});
+        let onLoad = jest.fn();
+        let onUnload = jest.fn();
+        options.onLoad = onLoad;
+        options.onUnload = onUnload;
+
+        options = observer(mapState)(options);
+        expect(options.onLoad).not.toBe(onLoad);
+        expect(options.onUnload).not.toBe(onUnload);
+
+        options.onLoad();
+        options.onUnload();
+        expect(onLoad).toHaveBeenCalled();
+        expect(onUnload).toHaveBeenCalled();
     });
 });
