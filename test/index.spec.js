2e47d737de09ca49c5d4b4a85f6b5836dc9c81fa
jkzing <jingkai.zhao@foxmail.com>
2017-11-13 20:05:40 +0800

    test(core): fix related unit tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,11 @@
 import assert from 'assert';
 import {createStore, observer} from '../src';
+import StoreMgr from '../src/store';
 
 
 describe('mobx-weapp main', () => {
-    let oldPage;
-    let oldApp;
     let store;
     beforeEach(() => {
-        global.Page = jest.fn();
-        global.App = jest.fn();
-        oldPage = Page;
-        oldApp = App;
         store = {};
         createStore(store);
     });
@@ -19,25 +14,7 @@ describe('mobx-weapp main', () => {
         expect(typeof createStore).toBe('function');
         expect(typeof observer).toBe('function');
     });
-    it('creates proxy method for global Page', () => {
-        expect(Page).not.toBe(oldPage);
-        Page({});
-        expect(oldPage).toHaveBeenCalled();
+    it('creates and saves store properly', () => {
+        expect(StoreMgr.getStore()).toBe(store);
     });
-    it('creates proxy method for global App', () => {
-        expect(App).not.toBe(oldApp);
-        App({});
-        expect(oldApp).toHaveBeenCalled();
-    });
-    it('adds alias $store for store on Page options', () => {
-        let options = {};
-        Page(options);
-        expect(options.$store).toBeDefined();
-        expect(options.$store).toBe(store);
-    });
-    it('creates empty Page options if it is undefined', () => {
-        expect(() => {
-            Page();
-        }).not.toThrow();
-    })
 })
